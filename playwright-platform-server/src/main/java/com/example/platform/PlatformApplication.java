2c6959cb8098c9bdfa7bf1fd93e405170c535b36
bytedance <xuzihao.404@bytedance.com>
2026-06-18 14:47:25 +0800

    feat: add mybatis bootstrap

--- playwright-platform-server/src/main/java/com/example/platform/PlatformApplication.java
+++ playwright-platform-server/src/main/java/com/example/platform/PlatformApplication.java
@@ -1,11 +1,13 @@
 package com.example.platform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan("com.example.platform.**.mapper")
 public class PlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlatformApplication.class, args);
